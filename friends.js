a399d58073b80e22a77d04ff148104ecd66ec2ff
Ben Rosner <ben.rosner.williamsburg@gmail.com>
2023-05-17 19:56:10 -0400

    Run second array variable

--- friends.js
+++ friends.js
@@ -2,3 +2,6 @@ var arrays = ["Ben", "Gavin", "Nicole", "Deanna", "Kaitlyn"];
 console.log(arrays[2]);
 console.log(arrays[4]);
 console.log(arrays[0]);
+
+var places = ["Denver", "New York", "Paris", "London", "Italy"];
+console.log(places.slice(4));
